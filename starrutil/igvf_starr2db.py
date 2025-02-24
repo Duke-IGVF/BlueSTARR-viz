fd5512e9701f734e2ab694beadebc2b3cdaf620b
Hilmar Lapp <hilmar.lapp@duke.edu>
2025-02-23 22:43:54 -0500

    Adds NumPy-formatted docstring to API function

--- starrutil/igvf_starr2db.py
+++ starrutil/igvf_starr2db.py
@@ -4,6 +4,26 @@ import pandas as pd
 from dbutil import relation_to_pq
 
 def starr2db(input_file: Path | str, db_file: Path | str, verbose: bool = False) -> None:
+    """
+    Converts a STARRseq sequence bins file to a Parquet database. The database
+    will be partitioned by chromosome, and experiment-speficic prefixes for RNA
+    and log2FC columns will be removed so that columns are named consistently.
+
+    Parameters
+    ----------
+    input_file : Path or str
+        Path to the input file containing STARRseq sequence bins and their
+        activations. The file can (but is not required to) be compressed with gzip.
+    db_file : Path or str
+        Path to the output Parquet database file. An already existing database will
+        be deleted rather than appended to.
+    verbose : bool, optional
+        If True, prints progress information (default is False).
+
+    Returns
+    -------
+    None
+    """
     if verbose:
         print(f"Parsing/loading input file: {input_file}")
     if isinstance(input_file, Path):
@@ -23,7 +43,7 @@ def starr2db(input_file: Path | str, db_file: Path | str, verbose: bool = False)
         print(f"Writing to Parquet database: {db_file}")
     relation_to_pq(df, db_file, partition_cols=['chrom'], existing_data_behavior='delete_matching')
 
-def main():
+def _main():
     parser = argparse.ArgumentParser(
         description='Converts a file with STARRseq sequence bins and their activations as input file to a Parquet database as output.')
     parser.add_argument('--input', '-i', required=True, help='Path to the input file')
@@ -35,4 +55,4 @@ def main():
     starr2db(args.input, args.db, args.verbose)
 
 if __name__ == "__main__":
-    main()
+    _main()
